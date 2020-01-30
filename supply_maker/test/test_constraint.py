4895a350475a89463cd22eef63f8db9b743981f4
Loui Sakaki <e1352207@outlook.jp>
2020-01-30 21:18:40 +0900

    updated

--- supply_maker/test/test_constraint.py
+++ supply_maker/test/test_constraint.py
@@ -9,8 +9,13 @@ from ..src.model.constraint import comply_with_constraint
 card_set = load_cards()
 
 
-# noinspection PyPep8Naming
 def test(N=100):
+    test_single(N)
+    test_and(N)
+    _flush_and_wait()
+
+
+def test_single(N):
     # noinspection PyProtectedMember
     def _inner(*, ac_counts, desc, **kwargs):
         for _ in tqdm(range(N), desc=desc):
@@ -29,9 +34,13 @@ def test(N=100):
             assert constraint(supply) == (num_of_compatible_cards in ac_counts)
 
         print('OK')
-        _flush_and_wait()
 
     _inner(
         ac_counts=[0, 3, 4, 5], ex='錬金術',
-        desc='錬金術師が0枚、あるいは3-5枚'
+        desc='Testing constraint; 0,3-5 from Alchemy'
     )
+
+
+def test_and(N):
+    # todo: implement
+    pass
