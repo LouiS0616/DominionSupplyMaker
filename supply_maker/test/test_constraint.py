00bab1d18aa6b1ec73764179dff2d29a5d1f3352
Loui Sakaki <e1352207@outlook.jp>
2020-01-30 22:09:54 +0900

    refactor: re-format printout

--- supply_maker/test/test_constraint.py
+++ supply_maker/test/test_constraint.py
@@ -28,7 +28,7 @@ def test_parse():
 
     def _inner(src: str, dst: List[int]):
         assert parse_constraint(src) == dst
-        print(f'OK -> {src}')
+        print(f'OK -> {src:10s} => {dst}')
 
     _inner('*', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     _inner('5', [5])
