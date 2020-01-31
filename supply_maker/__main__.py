34aa4b3a309a0422e94144cfde6531085cb30a32
Loui Sakaki <e1352207@outlook.jp>
2020-01-31 20:05:31 +0900

    add commandline args

--- supply_maker/__main__.py
+++ supply_maker/__main__.py
@@ -1,6 +1,30 @@
+import argparse
+
+from . import _where
 from .src.model.load import load_cards
 from .src.model.card_set import Supply
 
+
+def _init_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--constraint', nargs='?',
+        help='you can specify a yaml file which control supply balance.',
+
+        type=argparse.FileType('r', encoding='utf-8'),
+        default=(_where / 'res/constraints.yml').open(encoding='utf-8')
+    )
+
+    return parser
+
+
+def _main():
+    parser = _init_parser()
+    args = parser.parse_args()
+    # print(args.constraint)
+
+
+_main()
 
 card_set = load_cards()
 while True:
