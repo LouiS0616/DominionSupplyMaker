adbf68f67391ec504296cada1a3d9fd441c58ea0
Loui Sakaki <e1352207@outlook.jp>
2020-02-16 21:43:07 +0900

    make class; Candidates

--- supply_maker/src/model/preparation/_set_upper_impl.py
+++ supply_maker/src/model/preparation/_set_upper_impl.py
@@ -6,14 +6,14 @@ from .role import Role
 from ._set_upper import register_set_upper
 
 if TYPE_CHECKING:
-    from supply_maker.src.model.card_set import CardSet
+    from supply_maker.src.model.card_set import Candidates
     from ._set_upper import SupplySetUpper
 
 
 #
 @register_set_upper('Young Witch')
 def _setup_by_young_witch(
-        add_cards, candidates: 'CardSet',
+        add_cards, candidates: 'Candidates',
         card_to_role: Dict['Card', 'Role'], _: List[str]) -> None:
 
     bane = candidates.filter(
