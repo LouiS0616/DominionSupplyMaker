5ee2975cf4eec34ab80fba49d19da426ea16f948
Loui Sakaki <e1352207@outlook.jp>
2020-02-16 21:43:23 +0900

    remove driver

--- supply_maker/driver.py
+++
@@ -1,8 +0,0 @@
-from .src.model.card.attr.expansion_name import ExpansionName
-from .src.translation import Lang, set_lang
-
-
-from .src.model.card.attr.card_name import CardName
-
-set_lang(Lang.JP)
-print(CardName('Moat').t())
